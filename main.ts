59d3d6f36c174ecf53ac056fc564146cf1303ee6
Øystein Salomonsen <oystein.salomonsen@tekniskmuseum.no>
2021-12-09 00:44:46 +0100

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -10,9 +10,7 @@ namespace SpriteKind {
 }
 controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
     let Frisk_Vaksinerte: number[] = []
-    game.splash("Friske: " + (Friskmeldte.length + Frisk_Vaksinerte.length) + "   Syke: " + (Sykemeldte.length + Syk_Vaksinerte.length))
-    game.splash("Vaksinerte: " + Vaksinerte.length + "   Døde: " + Dødsmeldte.length)
-    game.splash("Leger: " + Helsepersonell.length)
+    game.splash("Friske: " + (Friskmeldte.length + Frisk_Vaksinerte.length) + "   Syke: " + (Sykemeldte.length + Syk_Vaksinerte.length), "Vaksinerte: " + Vaksinerte.length + "   Døde: " + Dødsmeldte.length)
 })
 controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
     if (Helsepersonell.length < 10) {
@@ -37,19 +35,16 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     Sykemeldte.push(Innbygger)
 })
 sprites.onOverlap(SpriteKind.Frisk, SpriteKind.Syk, function (sprite, otherSprite) {
-    TilfeldigTallSyk = randint(0, 100)
-    if (SannsynlighetSyk > TilfeldigTallSyk) {
-        sprite.destroy()
-        Innbygger = sprites.create(img`
-            2 2 2 2 
-            2 2 2 2 
-            2 2 2 2 
-            2 2 2 2 
-            `, SpriteKind.Syk)
-        Innbygger.setPosition(otherSprite.x, otherSprite.y)
-        Friskmeldte.removeAt(Friskmeldte.indexOf(sprite))
-        Sykemeldte.push(Innbygger)
-    }
+    sprite.destroy()
+    Innbygger = sprites.create(img`
+        2 2 2 2 
+        2 2 2 2 
+        2 2 2 2 
+        2 2 2 2 
+        `, SpriteKind.Syk)
+    Innbygger.setPosition(otherSprite.x, otherSprite.y)
+    Friskmeldte.removeAt(Friskmeldte.indexOf(sprite))
+    Sykemeldte.push(Innbygger)
 })
 sprites.onOverlap(SpriteKind.Frisk, SpriteKind.Lege, function (sprite, otherSprite) {
     if (VaksineAktiv) {
@@ -64,10 +59,13 @@ sprites.onOverlap(SpriteKind.Frisk, SpriteKind.Lege, function (sprite, otherSpri
         Friskmeldte.removeAt(Friskmeldte.indexOf(sprite))
         Vaksinerte.push(Innbygger)
     }
+    pause(Oppdatering)
 })
 sprites.onOverlap(SpriteKind.Vaksinert, SpriteKind.Syk_Vaksinert, function (sprite, otherSprite) {
     TilfeldigTallSyk = randint(0, 100)
+    Inkubasjonstid = randint(1000, 5000)
     if (SannsynlighetSykFraVaksinert > TilfeldigTallSyk) {
+        pause(Inkubasjonstid)
         sprite.destroy()
         Innbygger = sprites.create(img`
             9 9 9 9 
@@ -79,10 +77,12 @@ sprites.onOverlap(SpriteKind.Vaksinert, SpriteKind.Syk_Vaksinert, function (spri
         Vaksinerte.removeAt(Vaksinerte.indexOf(sprite))
         Syk_Vaksinerte.push(Innbygger)
     }
+    pause(Oppdatering)
 })
 sprites.onOverlap(SpriteKind.Vaksinert, SpriteKind.Syk, function (sprite, otherSprite) {
     TilfeldigTallSyk = randint(0, 100)
     if (SannsynlighetSykVaksinert > TilfeldigTallSyk) {
+        pause(Inkubasjonstid)
         sprite.destroy()
         Innbygger = sprites.create(img`
             9 9 9 9 
@@ -94,6 +94,7 @@ sprites.onOverlap(SpriteKind.Vaksinert, SpriteKind.Syk, function (sprite, otherS
         Vaksinerte.removeAt(Vaksinerte.indexOf(sprite))
         Syk_Vaksinerte.push(Innbygger)
     }
+    pause(Oppdatering)
 })
 sprites.onOverlap(SpriteKind.Syk, SpriteKind.Lege, function (sprite, otherSprite) {
     TilfeldigTallFrisk = randint(0, 100)
@@ -120,21 +121,25 @@ sprites.onOverlap(SpriteKind.Syk, SpriteKind.Lege, function (sprite, otherSprite
         Sykemeldte.removeAt(Sykemeldte.indexOf(sprite))
         Dødsmeldte.push(Innbygger)
     }
+    pause(Oppdatering)
 })
 sprites.onOverlap(SpriteKind.Frisk, SpriteKind.Syk_Vaksinert, function (sprite, otherSprite) {
     TilfeldigTallSyk = randint(0, 100)
-    if (SannsynlighetSykFraVaksinertFrisk > TilfeldigTallSyk) {
+    Inkubasjonstid = randint(1000, 5000)
+    if (SannsynlighetSykFraVaksinertHvisFrisk > TilfeldigTallSyk) {
+        pause(Inkubasjonstid)
         sprite.destroy()
         Innbygger = sprites.create(img`
-            9 9 9 9 
-            9 2 2 9 
-            9 2 2 9 
-            9 9 9 9 
-            `, SpriteKind.Syk_Vaksinert)
+            2 2 2 2 
+            2 2 2 2 
+            2 2 2 2 
+            2 2 2 2 
+            `, SpriteKind.Syk)
         Innbygger.setPosition(otherSprite.x, otherSprite.y)
-        Vaksinerte.removeAt(Vaksinerte.indexOf(sprite))
-        Syk_Vaksinerte.push(Innbygger)
+        Friskmeldte.removeAt(Friskmeldte.indexOf(sprite))
+        Sykemeldte.push(Innbygger)
     }
+    pause(Oppdatering)
 })
 sprites.onOverlap(SpriteKind.Syk_Vaksinert, SpriteKind.Lege, function (sprite, otherSprite) {
     TilfeldigTallFrisk = randint(0, 100)
@@ -161,12 +166,14 @@ sprites.onOverlap(SpriteKind.Syk_Vaksinert, SpriteKind.Lege, function (sprite, o
         Syk_Vaksinerte.removeAt(Syk_Vaksinerte.indexOf(sprite))
         Dødsmeldte.push(Innbygger)
     }
+    pause(Oppdatering)
 })
 let LegeTid = 0
-let Dag = 0
+let TidSidenUtbrudd = 0
 let TilfeldigTallFrisk = 0
-let VaksineAktiv = false
+let Inkubasjonstid = 0
 let TilfeldigTallSyk = 0
+let VaksineAktiv = false
 let Doctor: Sprite = null
 let Innbygger: Sprite = null
 let Dødsmeldte: Sprite[] = []
@@ -175,21 +182,22 @@ let Vaksinerte: Sprite[] = []
 let Helsepersonell: Sprite[] = []
 let Sykemeldte: Sprite[] = []
 let Friskmeldte: Sprite[] = []
-let SannsynlighetSykFraVaksinertFrisk = 0
+let SannsynlighetSykFraVaksinertHvisFrisk = 0
 let SannsynlighetSykFraVaksinert = 0
 let SannsynlighetSykVaksinert = 0
-let SannsynlighetSyk = 0
 let SannsynlighetFriskVaksinert = 0
 let SannsynlighetFrisk = 0
-let Dager = 1
+let Oppdatering = 0
 let DagLengde = 3000
+let AntallDager = 0
+Oppdatering = 100
 let Vaksineutviklingstid = 10
 SannsynlighetFrisk = 93
 SannsynlighetFriskVaksinert = 99
-SannsynlighetSyk = 75
+let SannsynlighetSyk = 75
 SannsynlighetSykVaksinert = 20
 SannsynlighetSykFraVaksinert = 10
-SannsynlighetSykFraVaksinertFrisk = 35
+SannsynlighetSykFraVaksinertHvisFrisk = 35
 let LegeInterval = 3
 Friskmeldte = sprites.allOfKind(SpriteKind.Frisk)
 Sykemeldte = sprites.allOfKind(SpriteKind.Syk)
@@ -216,13 +224,13 @@ Innbygger = sprites.create(img`
 Innbygger.setPosition(randint(0, 160), randint(0, 120))
 Sykemeldte.push(Innbygger)
 forever(function () {
-    if (game.runtime() > DagLengde * Vaksineutviklingstid) {
-        VaksineAktiv = true
+    if (game.runtime() > TidSidenUtbrudd + DagLengde) {
+        TidSidenUtbrudd = game.runtime()
+        AntallDager += 1
+        info.setScore(AntallDager)
     }
-    if (game.runtime() > Dag + DagLengde) {
-        Dag = game.runtime()
-        Dager += 1
-        info.setScore(Dager)
+    if (AntallDager > Vaksineutviklingstid) {
+        VaksineAktiv = true
     }
     if (game.runtime() > LegeTid + LegeInterval * DagLengde) {
         if (Helsepersonell.length < 10) {
@@ -243,7 +251,7 @@ forever(function () {
         game.over(true)
     }
 })
-game.onUpdateInterval(100, function () {
+game.onUpdateInterval(Oppdatering, function () {
     for (let Innbygger of Friskmeldte) {
         Innbygger.setPosition(Innbygger.x + randint(-2, 2), Innbygger.y + randint(-2, 2))
         Innbygger.setStayInScreen(true)
